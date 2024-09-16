bdf94dce8b2571ea9e8c6a327479f24827c43372
louism7032 <michaeljeanlouis655@gmail.com>
2024-09-16 11:24:16 -0400

    first upload

---
+++ P1LAB1_louismichael.py=completed.py
@@ -0,0 +1,13 @@
+#michael
+# 9/16/2024
+# P1lab1
+# get input from user and display
+
+# get first name from user
+first_name = input("Enter your first name; ")
+
+# get the last name from the user
+last_name = input("Enter your last name; ")
+
+# display info to user
+print("hello,",first_name, last_name + "!","welcome to CTI-110")
